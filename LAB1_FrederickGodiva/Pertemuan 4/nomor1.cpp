1b08d5e8276969734996d8836f481f0a71b8ea84
AlfaridziFarrelAkbar_231401028 <147838526+Alfaridziakbar@users.noreply.github.com>
2024-05-31 20:55:57 +0700

    nomor1.cpp

--- LAB1_FrederickGodiva/Pertemuan 4/nomor1.cpp
+++ LAB1_FrederickGodiva/Pertemuan 4/nomor1.cpp
@@ -3,22 +3,17 @@ using namespace std;
 
 main(){
     int i,j,n,suku=1;
-    //? Input panjang deret
     cout << "Input : "; cin >> n;
-
-    //? Deklarasi suku pertama
     cout << suku << " ";
-
-    //? Start dari sukur ke-2
     for(i=2; i<=n; i++){
-        //? Jika urutannya genap, maka selisihnya 5
         if (i % 2 == 0){
             suku += 5;
-        }else{
-            suku +=3; //? Jika urutannya ganjil, maka selisihnya 3
+        }
+        else
+        {
+            suku +=3;
         }
 
-        //? Tampilkan suku
         cout << suku << " ";
     }
 }
